2242750895f2892f416f789d561c2796bd89972f
KRLoyd <loydkristen@gmail.com>
2017-08-21 23:33:32 +0000

    Bash script to set up web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
-# sets up server for deployment
+# Bash script to set up a web server for the deployment of web_static
 
-sudo mkdir -p /data/web_static/releases/test/
-wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/deploy/test_index.html
-sudo mv test_index.html /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data -R
-wget https://raw.githubusercontent.com/glyif/AirBnB_clone_v2/master/deploy/config
-sudo mv config /etc/nginx/sites-available/default
-sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-sudo service nginx restart
+# Install Nginx if not installed
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo service nginx start
+# Create the folder /data/web_static/releases/test/ if it doesn't already exist
+mkdir -p /data/web_static/releases/test/
+# Create the folder /data/web_static/shared/ if it doesn't already exist
+mkdir -p /data/web_static/shared/
+# Create HTML file /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
+# If the link exists, delete and remake
+if [ -h '/data/web_static/current' ]
+then
+    rm /data/web_static/current
+    ln -s /data/web_static/releases/test/ /data/web_static/current
+else
+    ln -s /data/web_static/releases/test/ /data/web_static/current
+fi
+# Give ownership of the /data/ folder to the ubuntu user AND group, recursively
+chown -R ubuntu: /data/
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+TO_FIND="^server {$"
+TO_ADD="server {\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n"
+sudo sed -i "s/$TO_FIND/$TO_ADD/" /etc/nginx/sites-available/default
+sudo service nginx reload
