722769e7506bb8cb1fae02c9933f9eb696edad67
KRLoyd <loydkristen@gmail.com>
2017-09-06 16:13:42 +0000

    Work for Task 0:
    Create a function to route root ('/') using Flask.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Script to start a Flask web application.
+'''
+# Import necessary packages
+from flask import Flask
+
+# create an instance of Flask
+app = Flask(__name__)
+
+# map root to function
+@app.route('/')
+def hello_route(strict_slashes=False):
+    '''Function to return "Hello HBNB!" when requesting root '''
+    return 'Hello HBNB!'
+
+# run app if __main__
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
