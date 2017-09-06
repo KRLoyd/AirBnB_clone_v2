12ef34c8612e62d65c55a5c9edd466b3b8789065
KRLoyd <loydkristen@gmail.com>
2017-09-06 21:37:23 +0000

    Add a public getter method cities. Method is only for file_storage and returns a list

--- models/state.py
+++ models/state.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
-
+from models.engine.file_storage import all
 
 class State(BaseModel, Base):
     """State class handles all application states"""
@@ -25,3 +25,14 @@ class State(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """instantiates a new state"""
         super().__init__(self, *args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """"""
+            city_list = []
+            cities_dict = all(cls="City")
+            for k,v in cities_dict.items():
+                if v.get("state_id") == self.id:
+                    city_list.append(v)
+            return city_list
